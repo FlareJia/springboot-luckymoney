c7bfc4385e76f5eef066cdd234dd0e68c9681901
zhengjia <zhengjia@192.168.0.102>
2019-08-10 01:13:43 +0800

    springboot mysql jpa

---
+++ src/main/java/com/wlarein/luckymoney/LuckymoneyRepository.java
@@ -0,0 +1,7 @@
+package com.wlarein.luckymoney;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LuckymoneyRepository extends JpaRepository<Luckymoney, Integer> {
+
+}
