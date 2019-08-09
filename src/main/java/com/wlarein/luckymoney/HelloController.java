9052620b007e4bf43bc02a5b19725d84224bd657
zhengjia <zhengjia@192.168.0.102>
2019-08-09 23:01:09 +0800

    new commit

--- src/main/java/com/wlarein/luckymoney/HelloController.java
+++ src/main/java/com/wlarein/luckymoney/HelloController.java
@@ -2,19 +2,28 @@ package com.wlarein.luckymoney;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 
     @Autowired
     private LimitConfig limitConfig;
 
-    @GetMapping("/hello")
-    public String say(){
-        return "说明："+ limitConfig.getDescription();
+    // 这种使用 /say/100 来访问
+    @GetMapping("/say/{id}")
+    public String say(@PathVariable("id") Integer id){
+        //return "说明："+ limitConfig.getDescription();
+        //return "index";
+        return "id: "+ id;
+    }
+    // 这种使用 /say?id=100 来访问
+    @GetMapping("/say")
+    public String say2(@RequestParam(value = "id", required = false, defaultValue = "0") Integer myId){
+        return "id: "+ myId;
     }
 }
