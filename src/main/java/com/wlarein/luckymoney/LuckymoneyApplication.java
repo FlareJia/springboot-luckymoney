2016bed18acf6327cfd7c069f4792806859d2ccc
zhengjia <zhengjia@192.168.0.102>
2019-08-09 20:51:52 +0800

    initial

---
+++ src/main/java/com/wlarein/luckymoney/LuckymoneyApplication.java
@@ -0,0 +1,13 @@
+package com.wlarein.luckymoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckymoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckymoneyApplication.class, args);
+    }
+
+}
